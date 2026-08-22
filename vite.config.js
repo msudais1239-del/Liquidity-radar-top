653d63a76a1dbe734060cf0d528111a8bde2a90e
msudais1239-del <msudais1239@gmail.com>
2026-08-23 01:42:23 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-build: {
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, 'index.html'),
-      login:
-    },
-  },
-},
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
